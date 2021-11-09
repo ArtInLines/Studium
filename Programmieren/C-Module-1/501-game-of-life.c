d399306db6616dd0e628f3dae56a6bc222611987
ArtInLines <linesinart@gmail.com>
2021-11-09 16:20:58 +0100

    more

--- Programmieren/C-Module-1/501-game-of-life.c
+++ Programmieren/C-Module-1/501-game-of-life.c
@@ -21,20 +21,22 @@ see: http://en.wikipedia.org/wiki/Conway's_Game_of_Life
 // TO DO: Complete the programm such that it simulates the game of life. 
 //        Do this as teamwork (e.g. in teams with 2 or 3) 
 //        and benefit from the fact the functions can developed separately and then integrated into the final program.
-// TO DO optional 1: extend the program, such that it detects 'stable states', i.e. the system is oscillating between a few states. 
-// TO DO optional 2: let the program find a start state such that the system stays alive and unstable for as long as possible
-// TO DO optional 3: Create a flicker-free output: Do not print each character separately, but write the output into a string, which is printed all at once
-// TO DO optional 4: extend the program such that the content of the cells can be edited by the user. 
+// TODO optional 1: extend the program, such that it detects 'stable states', i.e. the system is oscillating between a few states. 
+// TODO optional 2: let the program find a start state such that the system stays alive and unstable for as long as possible
+// TODO optional 3: Create a flicker-free output: Do not print each character separately, but write the output into a string, which is printed all at once
+// TODO optional 4: extend the program such that the content of the cells can be edited by the user. 
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <Windows.h>
 
 #define HEIGHT 30
-#define LENGTH 30
+#define LENGTH 50
 
 // Global 2-dim-array which contains the cells
 char cells[HEIGHT][LENGTH];
+
 
 // TO DO: initialize cells, set most to 0, some to 1 
 void initialize_cells() {
@@ -42,49 +44,59 @@ void initialize_cells() {
    srand(time(NULL));
    for (i = 0; i < HEIGHT; i++) {
       for (j = 0; j < LENGTH; j++) {
-         if (rand() % 5) cells[i][j] = 0;
+         if (rand() % 8) cells[i][j] = 0;
          else cells[i][j] = 1;
          }
       }
    }
 
+
 // TO DO: Write output function to show the cells 
 void display_cells() {
    int i, j;
+   char out[HEIGHT * LENGTH + HEIGHT + 1];
    system("CLS"); // Clear screen - works (at least) on windows console.
    for (i = 0; i < HEIGHT; i++) {
       for (j = 0; j < LENGTH; j++) {
-         if (!cells[i][j]) printf(" ");
-         else printf("+");
+         if (!cells[i][j]) out[LENGTH * i + j] = ' ';
+         else out[LENGTH * i + j] = '+';
          }
-      printf("\n");
+      out[LENGTH * (i + 1) - 1] = '\n';
       }
+   printf("%s", out);
    }
+
 
 // TO DO: Write a function to calculate the next evolution step
 void evolution_step() {
    // TO DO: Use this array for the calculation of the next step
-   /*
-   1. Any live cell with fewer than two live neighbours dies, as if by needs caused by underpopulation.
-   2. Any live cell with more than three live neighbours dies, as if by overcrowding.
-   3. Any live cell with two or three live neighbours lives, unchanged, to the next generation.
-   4. Any empty/dead cell with exactly three live neighbours cells will be populated with a living cell.
-   */
-   int i, j;
    char cells_helper[HEIGHT][LENGTH];
+   int i, j, neighbours_count;
+
+   // DEBUG:
    for (i = 0; i < HEIGHT; i++) {
       for (j = 0; j < LENGTH; j++) {
          // Get neighbour cells
-         // ...
-         char neighbours[4];
+         // If there is no cells on one side, it will default to 0 (to simulate that there is no cell)
+         neighbours_count = i > 0 ? cells[i - 1][j] : 0;
+         neighbours_count += j < LENGTH - 1 ? cells[i][j + 1] : 0;
+         neighbours_count += i < HEIGHT - 1 ? cells[i + 1][j] : 0;
+         neighbours_count += j > 0 ? cells[i][j - 1] : 0;
+
+         // The usage of cells_helper assumes that every cell is supposed t be evaluated at the same time
+         // Otherwise, cells_helper could practically be removed and replaced with cells
          if (cells[i][j]) {
-            // Rules 1, 2, 3
+            if (neighbours_count < 2 || neighbours_count > 3) cells_helper[i][j] = 0;
+            else cells_helper[i][j] = 1;
             }
-         else if (/* Rule 4 */)
+         else if (neighbours_count >= 3) cells_helper[i][j] = 1;
+         else cells_helper[i][j] = 0;
          }
       }
 
+   for (i = 0; i < HEIGHT; i++) for (j = 0; j < LENGTH; j++) cells[i][j] = cells_helper[i][j];
    }
+
 
 // TO DO: Write a function that counts the occupied cells
 int count_cells() {
@@ -92,6 +104,7 @@ int count_cells() {
    for (i = 0; i < HEIGHT; i++) for (j = 0; j < LENGTH; j++) count += cells[i][j];
    return count;
    }
+
 
 // Main program
 int main() {
@@ -101,8 +114,7 @@ int main() {
       display_cells();
 
       // Leave loop if there are no more occupied cells
-      if (count_cells() == 0)
-         break;
+      if (count_cells() == 0) break;
 
       printf("Press enter");
       getchar();
