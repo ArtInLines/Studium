3d96721a6cf82fa3ff85c83fc518c3f3a305d991
ArtInLines <linesinart@gmail.com>
2022-02-23 09:38:44 +0100

    Notiz-Datei besser strukturiert

--- Theoretische Informatik/Grundlagen-Logik/learning/l/test.pl
+++ Theoretische Informatik/Grundlagen-Logik/learning/l/test.pl
@@ -1,3 +1,13 @@
+BEI REKURSION GENERELL:
+
+1. Muss sich selbst aufrufen
+2. Input muss bei jedem Aufruf kleiner werden
+3. Base-Case
+
+
+
+BEISPIEL FÜR REKURSION
+
 fakultät(n) {
     if (n <= 1) 1;
     n * fakultät(n-1);
@@ -10,16 +20,20 @@ n = 2
 fakultät(1) = 1
 
 
+hier in Prolog:
+
 fakultät(X, Ergebnis) :- X <= 1, Ergebnis is 1.
 fakultät(X, Ergebnis) :- Y is X-1, fakultät(Y, Z), Ergebnis is X * Z.
 
-fakultät(3, Ergebnis)
-
+":-" ist ein wenn-dann, das man von rechts nach links lesen muss:
 if X <= 1 and if Ergebnis is 1 then fakultät(X, Ergebnis):
 
-Ergebnis = 1;
 
 
+
+AUFGABE 8 - UNIFIZIEREN:
+
+Beispiel für Listen & Restlisten:
 [1, 2, 3] = [1 | [2, 3]]
 
 [X | R] = [1, 2, 3]
@@ -39,31 +53,26 @@ Y = 2
 Z = ?????
 
 
-
+a) 
 p1([[X|Y]|[X,Y]],[[Z]|X]) = p1([A|[B|B]],C)
 
+Beispiel:
 [1, 2, 3] = [1 | [2, 3]]
-
 [ X | [X, Y] ] = [X, X, Y]
-
 [1, 2, 3] = [X | R] = [1 | [2, 3]] = [X | [ Y | Z ]] = [1 | [2 | [3] ]]
-
-
 
 A = 1
 B = [2]
 [A | [B | B]] = [1, [2], 2]
 
+Lösung der Aufgabe:
 [ [X|Y] | [X,Y] ] = [A | [B|B]]
+// [ [X|Y], X, Y ] = [ A, B, B[0] ]
 
-[ [X|Y], X, Y ] = [ A, B, B[0] ]
-
+Das hier ist Hinzuschreiben:
 A <- [X|Y] = [ [Y] | Y ]
 B <-> X <- [Y]
-
 [[Z]|X] -> C <- [[Z] | [Y]] = [ [Z], Y ]
-
-
 
 
 b)
@@ -78,13 +87,19 @@ X <- []
 [X|R] -> Y1
 Y <-> A
 
+Das hier ist Hinzuschreiben:
 Y1 <-> Y <-> A <-> [X|R] <-> [[] | R]
 
 
-Die beiden einzigen Arten occur checks zu kriegen (ohne Gewähr):
-Y1 <-> Y <-> [Y] occur check!
-[X, Y] = [A] occur check!
+GENERELL FÜR AUFGABE 8:
 
+Die beiden einzigen Arten von occur checks (ohne Gewähr):
+- Y <-> [Y]
+- [X, Y] = [A]
+
+
+
+PROLOG FUNKTIONEN:
 
 conc:
 2 Listen zu einer Liste
@@ -94,9 +109,6 @@ conc(L1, [], L1).
 conc([], L2, L2).
 conc([X | R], L2, [X | Z]) :- conc(R, L2, Z).
 
-1. Muss sich selbst aufrufen
-2. Input muss bei jedem Aufruf kleiner werden
-3. Base-Case
 
 
 zipper:
@@ -108,12 +120,14 @@ zipper([], L2, L2).
 zipper(L1, [], L1).
 zipper([X1|R1], [X2|R2], [X1, X2 | Z]) :- zipper(R1, R2, Z)
 
+
 L1 = [1, 2, 3], L2 =[4, 5, 6, 7, 8]
 X1 = 1
 X2 = 4
 R1 = [2, 3]
 R2 = [5, 6, 7, 8]
 zipper(R1, R2, T) => Z = [1, 4, 2, 5, 3, 6, 7, 8]
+
 
 
 rev:
@@ -124,6 +138,7 @@ rev([], []).
 rev([X], [X]).
 rev([X|R], Z) :- rev(R, U), conc(U, [X], Z).
 
+
 rev([1, 2, 3], Ergebnis) = [3, 2, 1]
 X = 1
 R = [2, 3]
@@ -132,8 +147,8 @@ X = 2
 R = [3]
 U = rev([3], U) = [3]
 
-[X]
-[X|R] 
+
+
 
 TODO:
 
