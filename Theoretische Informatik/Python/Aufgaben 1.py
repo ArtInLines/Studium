56efbb8d7ebeb347ac1ced116d162df2043baaa8
ArtInLines <linesinart@gmail.com>
2022-05-04 15:51:00 +0200

    t

--- Theoretische Informatik/Python/Aufgaben 1.py
+++ Theoretische Informatik/Python/Aufgaben 1.py
@@ -1,3 +1,6 @@
+def teiler(n: int):
+	return { x for x in range(1, n+1) if n % x == 0 }
+
 def echteTeiler(n: int):
 	return { x for x in range(1, n) if n % x == 0 }
 	
@@ -14,30 +17,21 @@ def isPerfect(n: int):
 # print(allePerfekten)
 
 def gt(m: int, n: int):
-	return echteTeiler(m) & echteTeiler(n)
-
-def gt(a, b, c):
-	return echteTeiler(a) & echteTeiler(b) & echteTeiler(c)
+	return teiler(m) & teiler(n)
 
 # for i in range(1, 20):
 # 	for j in range(1, 20):
 # 		print(i, j, gt(i, j))
 
 def ggt(m: int, n: int):
-	x = gt(m, n)
-	if len(x) == 0:
-		return 0
-	return max(x)
+	return max(gt(m, n))
 
 # for i in range(1, 20):
 # 	for j in range(1, 20):
 # 		print(i, j, ggt(i, j))
 
 def kgv(m: int, n: int):
-	g = ggt(m, n)
-	if g == 0:
-		return m * n
-	return int(abs((m / g) * n))
+	return m * n // ggt(m, n)
 
 # for i in range(1, 20):
 # 	for j in range(1, 20):
@@ -57,18 +51,17 @@ def power(M):
 # print(power({1, 2, 3}))
 
 def pythagoras(n: int):
-	return [(a, b, c) for c in range(5, n+1) for b in range(2, c) for a in range(1, b) if a**2 + b**2 == c**2]
+	return {(a, b, c) for c in range(5, n+1) for b in range(2, c) for a in range(1, b) if a**2 + b**2 == c**2}
 
 # print(pythagoras(100))
 
-def isReduced(a: int, b: int, c: int):
-	return len(gt(a, b, c)) <= 1
-
 def reducedPythagoras(n: int):
-	return [(a, b, c) for (a, b, c) in pythagoras(n) if isReduced(a, b, c)]
+	return {(a, b, c) for (a, b, c) in pythagoras(n) if ggt(a, b) == 1}
 
 # print(reducedPythagoras(50))
 
+# Diese sind falsch!!
+# Siehe Lsg. von Herrn Stroetmann
 def aufgabe6A():
 	return  3 * ((11 / 50) * (10 / 49) * (10 / 48)) + ((11 / 50) * (10 / 49) * (9 / 48))
 def aufgabe6B():
@@ -88,7 +81,7 @@ def anagram(s: str):
 # 		return {s}
 # 	return { s[i] + s1 for i in range(len(s)) for s1 in anagram(s[:i] + s[i+1:]) }
 
-print(anagram("abc"))
+# print(anagram("abc"))
 
 def numberDiceRolls(n: int, s: int):
 	if n == 0 or s == 0:
