4166e6e7731677e6d4814f8ced8595e3bc120a81
ArtInLines <linesinart@gmail.com>
2022-05-23 15:19:15 +0200

    t

--- Java-Programmierung/Task3/Student.java
+++ Java-Programmierung/Task3/Student.java
@@ -3,8 +3,25 @@ import java.util.Calendar;
 public class Student extends Person {
 	private String subject;
 	private Calendar immatriculationDate;
-	Student(String name, String firstName, int birthyear, int birthmonth, int birthday) {
+	Student(String name, String firstName, int birthyear, int birthmonth, int birthday, String subject, Calendar immatriculationDate) {
 		super(name, firstName, birthyear, birthmonth, birthday);
-		
+		this.subject = subject;
+		this.immatriculationDate = immatriculationDate;
+	}
+	
+	public String getSubject() {
+		return subject;
+	}
+	
+	public Calendar getImmatriculationDate() {
+		return immatriculationDate;
+	}
+	
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	
+	public void setImmatriculationDate(Calendar immatriculationDate) {
+		this.immatriculationDate = immatriculationDate;
 	}
 }
